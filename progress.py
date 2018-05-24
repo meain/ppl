601d36166ef6993ba3528ab6374a31efff6fd8b4
Abin Simon <abinsimon10@gmail.com>
2018-05-25 02:22:40 +0530

    fix color names

--- progress.py
+++ progress.py
@@ -5,19 +5,20 @@ import random
 
 
 class bcolors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
+    PINK = '\033[95m'
+    BLUE = '\033[94m'
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    RED = '\033[91m'
     ENDC = '\033[0m'
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
 
 
-def pb(iterable, task=''):
+def pb(iterable, task='Task', bar_len=0):
     rows, columns = [int(i) for i in os.popen('stty size', 'r').read().split()]
-    bar_len = columns - 10
+    if bar_len == 0:
+        bar_len = columns - 10
     CURSOR_UP_ONE = '\x1b[1A'
     ERASE_LINE = '\x1b[2K'
 
@@ -33,14 +34,14 @@ def pb(iterable, task=''):
         if count == 1:
             times = time.time()
             sys.stdout.write(ERASE_LINE)
-            print(bcolors.FAIL + '• ' + task + bcolors.ENDC)
+            print(bcolors.RED + '• ' + task + bcolors.ENDC)
 
         if count == total - 1:
             time_taken = time.time() - times
             sys.stdout.write(ERASE_LINE)
             sys.stdout.write(CURSOR_UP_ONE)
             sys.stdout.write(ERASE_LINE)
-            print(bcolors.OKGREEN + '✔ ' + task + bcolors.ENDC + ' took ' +
+            print(bcolors.GREEN + '✔ ' + task + bcolors.ENDC + ' took ' +
                   str(int(time_taken)) + 's')
         else:
             bar = ' ' * 2 + '─' * (filled_len - 1) + '•' + '⋯' * (
@@ -50,14 +51,14 @@ def pb(iterable, task=''):
             sys.stdout.flush()
 
 
-total = 120
-tasks = [
-    'Make paintball', 'Find dragons', 'Code in python', 'Take out the trash',
-    'Fill up water bottles for trip'
-]
-
-for task in tasks:
-    i = 0
-    for i in pb(range(total), task=task):
-        sleep_time = [.05, .04, .03, .02, .01][random.randint(0, 4)]
-        time.sleep(sleep_time)  # emulating long-playing task
+if __name__ == '__main__':
+    total = 120
+    tasks = [
+        'Make paintball', 'Find dragons', 'Code in python', 'Take out the trash',
+        'Fill up water bottles for trip'
+    ]
+    for task in tasks:
+        i = 0
+        for i in pb(range(total), task=task):
+            sleep_time = [.05, .04, .03, .02, .01][random.randint(0, 4)]
+            time.sleep(sleep_time)  # emulating long-playing task
