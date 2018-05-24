44c5e312f3cedcd9923a792f180d3caebfd7ced3
Abin Simon <abinsimon10@gmail.com>
2018-05-25 02:13:17 +0530

    work like tqdm

--- progress.py
+++ progress.py
@@ -18,32 +18,39 @@ class bcolors:
 times = time.time()
 
 
-def progress(count, total, task=''):
+def pb(iterable, task=''):
     global times
     rows, columns = [int(i) for i in os.popen('stty size', 'r').read().split()]
     bar_len = columns - 10
     CURSOR_UP_ONE = '\x1b[1A'
     ERASE_LINE = '\x1b[2K'
-    filled_len = int(round(bar_len * count / float(total)))
 
-    percents = round(100.0 * count / float(total), 1)
+    count = 0
+    total = len(iterable)
+    for obj in iterable:
+        yield obj
+        count += 1
+        filled_len = int(round(bar_len * count / float(total)))
 
-    if count == 0:
-        times = time.time()
-        print(bcolors.FAIL + '• ' + task + bcolors.ENDC)
+        percents = round(100.0 * count / float(total), 1)
 
-    if count == total - 1:
-        time_taken = time.time() - times
-        sys.stdout.write(ERASE_LINE)
-        sys.stdout.write(CURSOR_UP_ONE)
-        sys.stdout.write(ERASE_LINE)
-        print(bcolors.OKGREEN + '✔ ' + task + bcolors.ENDC + ' took ' +
-              str(int(time_taken)) + 's')
-    else:
-        bar = ' ' * 2 + '─' * (filled_len - 1) + '•' + '⋯' * (
-            bar_len - filled_len)
-        sys.stdout.write('%s %s%s%s\r' % (bar, percents, '%', bcolors.ENDC))
-        sys.stdout.flush()
+        if count == 1:
+            times = time.time()
+            sys.stdout.write(ERASE_LINE)
+            print(bcolors.FAIL + '• ' + task + bcolors.ENDC)
+
+        if count == total - 1:
+            time_taken = time.time() - times
+            sys.stdout.write(ERASE_LINE)
+            sys.stdout.write(CURSOR_UP_ONE)
+            sys.stdout.write(ERASE_LINE)
+            print(bcolors.OKGREEN + '✔ ' + task + bcolors.ENDC + ' took ' +
+                  str(int(time_taken)) + 's')
+        else:
+            bar = ' ' * 2 + '─' * (filled_len - 1) + '•' + '⋯' * (
+                bar_len - filled_len)
+            sys.stdout.write('%s %s%s%s\r' % (bar, percents, '%', bcolors.ENDC))
+            sys.stdout.flush()
 
 
 total = 120
@@ -51,10 +58,16 @@ tasks = [
     'Make paintball', 'Find dragons', 'Code in python', 'Take out the trash',
     'Fill up water bottles for trip'
 ]
+
 for task in tasks:
     i = 0
-    while i < total:
-        progress(i, total, task=task)
-        sleep_time = [.05, .04, .03, .02, .01][random.randint(0, 4)]
-        time.sleep(sleep_time)  # emulating long-playing job
-        i += 1
+    for i in pb(range(total), task=task):
+        time.sleep(0.01)
+
+# for task in tasks:
+#     i = 0
+#     while i < total:
+#         progress(i, total, task=task)
+#         sleep_time = [.05, .04, .03, .02, .01][random.randint(0, 4)]
+#         time.sleep(sleep_time)  # emulating long-playing job
+#         i += 1
