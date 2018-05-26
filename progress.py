f6d237083000d1f958cbdb6c5670e36e7be52f27
Abin Simon <abinsimon10@gmail.com>
2018-05-27 02:27:18 +0530

    housekeeping

--- progress.py
+++ progress.py
@@ -2,6 +2,7 @@ import os
 import sys
 import time
 import types
+import math
 
 
 class bcolors:
@@ -26,16 +27,15 @@ def _draw_progress_bar(task, total, count, start_time, bar_len):
     time_left = (total - count) * avg_time
     count_per_sec = 1 / avg_time
 
-    if count < total - 1:
-        sys.stdout.write(ERASE_LINE)
-        print('%s• %s%s' % (bcolors.RED, task, bcolors.ENDC))
-
     if count == total - 1:
         time_taken = time.time() - start_time
         sys.stdout.write(ERASE_LINE)
         print('%s✔ %s%s  took: %.2fs\r' % (bcolors.GREEN, task, bcolors.ENDC,
                                            time_taken))
     if count < total - 1:
+        sys.stdout.write(ERASE_LINE)
+        print('%s• %s%s' % (bcolors.RED, task, bcolors.ENDC))
+
         bar = ' ' * 2 + '─' * (filled_len - 1) + '•' + '⋯' * (
             bar_len - filled_len)
         sys.stdout.write(ERASE_LINE)
@@ -43,7 +43,6 @@ def _draw_progress_bar(task, total, count, start_time, bar_len):
             '%s  %.2f%s%s\r\n' % (bar, percents, '%', bcolors.ENDC))
         sys.stdout.flush()
 
-    if count < total - 1:
         print('    %s avg: %.2fs  %sleft: %.2fs  %scount: %.2fiter/s%s' %
               (bcolors.PINK, avg_time, bcolors.BLUE, time_left, bcolors.YELLOW,
                count_per_sec, bcolors.ENDC))
@@ -75,16 +74,13 @@ def _draw_spinner(task, count, start_time, final=False):
 
 
 def pb(iterable, task='Task', bar_len=0):
-    CURSOR_UP_ONE = '\x1b[1A'
-    ERASE_LINE = '\x1b[2K'
-    rows, columns = [int(i) for i in os.popen('stty size', 'r').read().split()]
+    columns = int(os.popen('stty size', 'r').read().split()[1])
     if bar_len == 0:
         bar_len = columns - 13
     is_generator = isinstance(iterable, types.GeneratorType)
     count = 0
     start_time = time.time()
     if is_generator:
-        spinner_icons = ['◐', '◓', '◑', '◒']
         for obj in iterable:
             yield obj
             count += 1
