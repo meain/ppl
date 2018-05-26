9cedb1b12eb967f8dd2dcfd8457bc4fce44f8e30
Abin Simon <abinsimon10@gmail.com>
2018-05-27 02:52:03 +0530

    cleanup

--- progress.py
+++ progress.py
@@ -23,7 +23,11 @@ def _draw_progress_bar(task, total, count, start_time, bar_len):
     filled_len = int(round(bar_len * count / float(total)))
     percents = round(100.0 * count / float(total), 1)
 
-    avg_time = (time.time() - start_time) / count
+    spinner_icons = ['◐', '◓', '◑', '◒']
+    time_taken = time.time() - start_time
+    icon = spinner_icons[int(((time_taken * 10) % 4))]
+
+    avg_time = time_taken / count
     time_left = (total - count) * avg_time
     count_per_sec = 1 / avg_time
 
@@ -34,7 +38,7 @@ def _draw_progress_bar(task, total, count, start_time, bar_len):
                                            time_taken))
     if count < total - 1:
         sys.stdout.write(ERASE_LINE)
-        print('%s• %s%s' % (bcolors.RED, task, bcolors.ENDC))
+        print('%s%s %s%s' % (bcolors.RED, icon, task, bcolors.ENDC))
 
         bar = ' ' * 2 + '─' * (filled_len - 1) + '•' + '⋯' * (
             bar_len - filled_len)
@@ -43,9 +47,10 @@ def _draw_progress_bar(task, total, count, start_time, bar_len):
             '%s  %.2f%s%s\r\n' % (bar, percents, '%', bcolors.ENDC))
         sys.stdout.flush()
 
-        print('    %s avg: %.2fs  %sleft: %.2fs  %scount: %.2fiter/s%s' %
-              (bcolors.PINK, avg_time, bcolors.BLUE, time_left, bcolors.YELLOW,
-               count_per_sec, bcolors.ENDC))
+        print(
+            ' %s avg: %.2fs  %sleft: %.2fs  %siter: %d  %sspeed: %.2fiter/s%s'
+            % (bcolors.PINK, avg_time, bcolors.BLUE, time_left, bcolors.YELLOW,
+               count, bcolors.GREEN, count_per_sec, bcolors.ENDC))
         sys.stdout.write(CURSOR_UP_ONE)
         sys.stdout.write(CURSOR_UP_ONE)
         sys.stdout.write(CURSOR_UP_ONE)
@@ -57,13 +62,15 @@ def _draw_spinner(task, count, start_time, final=False):
     spinner_icons = ['◐', '◓', '◑', '◒']
     time_taken = time.time() - start_time
     avg_time = time_taken / count
+    count_per_sec = 1 / avg_time
     icon = spinner_icons[int(((time_taken * 10) % 4))]
     sys.stdout.write(ERASE_LINE)
     print('%s%s %s%s' % (bcolors.RED, icon, task, bcolors.ENDC))
     sys.stdout.write(ERASE_LINE)
-    sys.stdout.write('%s  avg: %.2fs%s  time: %.2fs  %siter: %d%s\r' %
-                     (bcolors.PINK, avg_time, bcolors.BLUE, time_taken,
-                      bcolors.YELLOW, count, bcolors.ENDC))
+    sys.stdout.write(
+        '%s  avg: %.2fs%s  time: %.2fs  %siter: %d  %sspeed: %.2fiter/s%s\r' %
+        (bcolors.PINK, avg_time, bcolors.BLUE, time_taken, bcolors.YELLOW,
+         count, bcolors.GREEN, count_per_sec, bcolors.ENDC))
     sys.stdout.write(ERASE_LINE)
     sys.stdout.write(CURSOR_UP_ONE)
     if final:
@@ -90,8 +97,10 @@ def _draw_mini_progress_bar(task, total, count, start_time):
                                            time_taken))
     if count < total - 1:
         sys.stdout.write(ERASE_LINE)
-        print('%s%s %s%s %.2f%s' % (bcolors.RED,icon, task, bcolors.ENDC, percents, '%'))
+        print('%s%s %s%s %.2f%s' % (bcolors.RED, icon, task, bcolors.ENDC,
+                                    percents, '%'))
         sys.stdout.write(CURSOR_UP_ONE)
+
 
 def _draw_mini_spinner(task, count, start_time, final=False):
     CURSOR_UP_ONE = '\x1b[1A'
@@ -100,7 +109,8 @@ def _draw_mini_spinner(task, count, start_time, final=False):
     time_taken = time.time() - start_time
     icon = spinner_icons[int(((time_taken * 10) % 4))]
     sys.stdout.write(ERASE_LINE)
-    print('%s%s %s%s iter: %d' % (bcolors.RED, icon, task, bcolors.ENDC, count))
+    print(
+        '%s%s %s%s iter: %d' % (bcolors.RED, icon, task, bcolors.ENDC, count))
     sys.stdout.write(CURSOR_UP_ONE)
     if final:
         total_time = time.time() - start_time
@@ -108,10 +118,8 @@ def _draw_mini_spinner(task, count, start_time, final=False):
         print('%s%s %s%s  took: %.2fs' % (bcolors.GREEN, '✔', task,
                                           bcolors.ENDC, total_time))
 
+
 def pb(iterable, task='Task', bar_len=0, mini=False):
-    columns = int(os.popen('stty size', 'r').read().split()[1])
-    if bar_len == 0:
-        bar_len = columns - 13
     is_generator = isinstance(iterable, types.GeneratorType)
     count = 0
     start_time = time.time()
@@ -131,6 +139,9 @@ def pb(iterable, task='Task', bar_len=0, mini=False):
     else:
         total = len(iterable)
         if not mini:
+            columns = int(os.popen('stty size', 'r').read().split()[1])
+            if bar_len == 0:
+                bar_len = columns - 13
             for obj in iterable:
                 yield obj
                 count += 1
