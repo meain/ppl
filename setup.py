69d6d32f24621b8d9c42568582003394eba87d58
Abin Simon <abinsimon10@gmail.com>
2018-09-09 11:42:09 +0530

    bump version number

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ cur_classifiers = [
 
 setuptools.setup(
     name="ppl",
-    version="0.2.0",
+    version="0.3.0",
     author='Abin Simon',
     author_email='abinsimon10@gmail.com',
     description="A pretty progressbar library",
